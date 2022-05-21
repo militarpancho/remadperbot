a7832e98bee9c0daec51a2aeb5628c58d4120428
Manuel Garcia-Amado <militarpancho_93@msn.com>
2022-05-21 13:44:13 +0200

    minor fix

--- db/users_item_update.go
+++ db/users_item_update.go
@@ -74,7 +74,7 @@ func (db Database) GetUserItemUpdateById(UserId string, ItemUpdateId string) (mo
 }
 
 func (db Database) DeleteUsersItemUpdate(ItemUpdateId string) error {
-	query := `DELETE FROM users_item_updates WHERE item_update_id = $2;`
+	query := `DELETE FROM users_item_updates WHERE item_update_id = $1;`
 	_, err := db.Conn.Exec(query, ItemUpdateId)
 	switch err {
 	case sql.ErrNoRows:
